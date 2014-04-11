afff1901a58e08b89140dc3c59b49fce50c088b1
alexanderorn <alexanderorn1@gmail.com>
2014-04-11 23:18:53 +0200

    started resourcemanager

---
+++ include/ResourceManager.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <SFML/Graphics/Texture.hpp>
+#include <string>
+#include <unordered_map>
+
+class ResourceManager
+{
+public:
+	static sf::Texture& GetTexture(const std::string &filename);
+	static bool LoadTextures(const std::string &filename);
+
+	static bool Load();
+	static void Unload();
+
+private:
+	ResourceManager();
+	
+	static bool LoadTextures(const std::string &filename);	
+	
+	static void UnloadTextures();
+
+	typedef std::unordered_map<std::string, sf::Texture*> TextureMap;
+	static TextureMap mTextureMap;
+
+	ResourceManager(const ResourceManager&);
+	ResourceManager& operator = (const ResourceManager&);
+};
---
+++ source/ResourceManager.cpp
@@ -0,0 +1,32 @@
+#include <ResourceManager.h>
+#include <iostream>
+
+sf::Texture& ResourceManager::GetTexture(const std::string &filename)
+{
+	TextureMap::iterator & it = mTextureMap.find( filename );
+
+	return *(it->second);
+}
+
+bool ResourceManager::LoadTextures( const std::string &filename ) 
+{
+	TextureMap::iterator it = mTextureMap.find( filename );
+
+	if( it != mTextureMap.end( ) )
+	{
+		return true;
+	}
+
+	sf::Texture * texture = new sf::Texture;
+	if(texture->loadFromFile( filename ) == false)
+	{
+		std::cout << "[ResourceManager.cpp]Failed to load sound file." << std::endl;
+		delete texture;
+		return false;
+	}
+	std::cout << "ok: " << filename << std::endl;
+	mTextureMap[filename] = texture;
+	return true;
+}
+
+
