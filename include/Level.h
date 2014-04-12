0626b1d513a60563e2f06e182075e9db8fcd53d9
JimmieBergmann <jimmiebergmann@gmail.com>
2014-04-12 17:02:30 +0200

    Added Json library

--- include/Level.h
+++ include/Level.h
@@ -8,17 +8,24 @@
 class Level
 {
 public:
-	Level( );
+	Level( const std::string & filename );
 	~Level( );
 	
 	void update( float delta );
 	void draw( sf::RenderTarget& target );
-	void load( );
 	void unload( );
+	inline bool isLoaded( ) const { return m_Loaded; }
 
 private:
-	Level( const Level& );
 
+	// Private functions
+	bool loadFromFile( const std::string & filename );
+
+	// Make it inpossible to copy this level
+	Level( const Level& );
+	Level & operator = ( const Level & );
+
+	bool m_Loaded;
 	b2World m_World;
 	Ninja* m_pNinja;
 	DynamicBody m_Platform;
--- source/Level.cpp
+++ source/Level.cpp
@@ -1,27 +1,21 @@
 #include <Level.h>
+#include <Json/reader.h>
+#include <Json/value.h>
+#include <fstream>
 #include <MemoryLeak.h>
 
 
-Level::Level( ):
+Level::Level( const std::string & filename ) :
+	m_Loaded( false ),
 	m_World(b2Vec2( 0.f, 9.82f ) ),
 	m_Platform(m_World, 100.0f, 300.0f, 0, 3, 0, 1, 1, true, 600, 200)
 {
-	m_pNinja = new Ninja( m_World );
-	m_pNinja->getBody()->setPosition(b2Vec2( 100 , 100));
-
-	m_PlatformShape.setPosition( m_Platform.getPosition().x , m_Platform.getPosition().y  );
-	m_PlatformShape.setSize( sf::Vector2f( 600.0f, 200) );
-	m_PlatformShape.setFillColor( sf::Color( 50, 50, 50, 255 ) );
+	loadFromFile( filename );
 }
 
 Level::~Level( )
 {
-	if( m_pNinja )
-	{
-		delete m_pNinja;
-		m_pNinja = NULL;
-	}
-
+	unload( );
 }
 
 void Level::update(float delta)
@@ -38,13 +32,59 @@ void Level::draw( sf::RenderTarget& target )
 	// Render the platform
 	target.draw( m_PlatformShape );
 }
-
-void Level::load()
-{
-
-}
-
 void Level::unload()
 {
+	if( m_pNinja )
+	{
+		delete m_pNinja;
+		m_pNinja = NULL;
+	}
+}
 
+bool Level::loadFromFile( const std::string & filename )
+{
+	// Load the ninja
+	m_pNinja = new Ninja( m_World );
+	m_pNinja->getBody()->setPosition(b2Vec2( 100 , 100));
+
+	// Add default platform
+	m_PlatformShape.setPosition( m_Platform.getPosition().x , m_Platform.getPosition().y  );
+	m_PlatformShape.setSize( sf::Vector2f( 600.0f, 200) );
+	m_PlatformShape.setFillColor( sf::Color( 50, 50, 50, 255 ) );
+	
+
+
+
+	// Open the file
+	std::ifstream fin( filename.c_str( ) );
+    if( !fin.is_open() )
+    {
+         return false;
+    }
+	
+	// Read the file
+	std::string input = "";
+    while( !fin.eof() )
+    {
+        // Get and add the line
+        std::string line;
+        std::getline( fin, line );
+        input += line + "\n";
+    }
+
+	// closes the file
+	fin.close();
+
+	// Get the root
+	Json::Value jsonRoot;
+	Json::Reader jsonReader;
+	if( jsonReader.parse( input, jsonRoot ) == false )
+	{
+		std::cout << "[Level::loadFromFile] Failed to parse level file." << std::endl;
+		return false;
+	}
+
+	// Succeeded
+	m_Loaded = true;
+	return true;
 }
